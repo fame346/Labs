c6e1f05cca17ed4e17952e1f156db390c63df116
Patrick Azzo <patrickazzo3@yahoo.com>
2019-09-26 21:42:14 -0400

    lab3.py

---
+++ Desktop/Fall 2019/COMP525/lab3/lab3.py
@@ -0,0 +1,39 @@
+"""
+lab3.py
+Patrick Azzo
+9/25/2019
+"""
+
+def my_find(word, letter):
+    """
+    Tests if letter is in word
+    word: string
+    letter: one-character string
+    """
+word = 'friendly'
+letter = 'i'
+
+#    str = "friendly"
+#    str.find('i')
+if letter in word:
+     print(f'yes')
+else:
+    print('no')
+      #returns {my_find('friendly','f')})
+
+
+def sum_to(n):
+    """
+    Calculates the sum of all positive integers from 1 to number, including
+    number.
+    Uses the accumulator pattern.
+    Do not use the formula that computes this sum.
+
+    n: non-negative integer
+    Returns: The sum of all numbers from 1 to n
+"""
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+accum = 0
+for i in n:
+   accum = accum + i
+print(accum)
